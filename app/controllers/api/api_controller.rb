c4878df4481327fd486f91200daeb67d97a46028
tontoro <siotontoro@yahoo.co.jp>
2015-10-19 00:35:53 +0000

    ミッション達成

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -68,25 +68,53 @@ class Api::ApiController < ApplicationController
                 end
                 
                 # そのミッションが割り当てられているかチェック
-                @assign = @user.assigns
-                @assign.each do |assign_id|
-                    assign = Assign.find_by(id: assign_id)
+                @mission_ids.each do |mission_id|
+                    #assigns = @user.assigns
+                    assign = @user.assigns.find_by(mission_id: mission_id)
                     unless assign
-                        error = { error:"404 Not Found",detail:"missions are not assigned with mission_ids=#{params[:mission_ids]}"}
-                        render json: error 
+                        error = {error:"404 Not Found",detail:"missions are not assigned with mission_ids=#{params[:mission_ids]}"}
+                        render json: error and return
                     end
                 end
                 
                 # 割り当ての達成状態をtrueに切り替える
-                @assign.achievement = true
+                @assigns = @user.assigns
+                @assigns.each do |assign|
+                    assign.achievement = true
+                    assign.save
+                end
+
                 # historyに達成データ書き込み
-                history = History.new
-                history.experience += acquisition.experience
                 
-                history.save
-                
-                # ユーザーの経験値増加
-                
+                # ユーザーのステータスを持ってくる 
+                statuses = @user.statuses
+                # mission_idsのループをまわす
+                @mission_ids.each do |mission_id|
+                    # ミッションの獲得を取り出す
+                    acquisitions = Mission.find_by(id: mission_id).acquisitions
+                    # 獲得ごとのループをまわす
+                    acquisitions.each do |acquisition|
+                        # ステータスの中から獲得のcategory_idと一致するものを取り出す
+                        status = statuses.find_by(category_id: acquisition.category_id)
+                        # 新しい経験値を計算 ← ステータスに入っている現在の経験値 + ミッションの経験値 
+                        experience = status.experience + acquisition.experience
+                        # ステータスの経験値をステータスの以前の経験値にコピー
+                        status.recent_experience = status.experience
+                        # 新しい経験値をステータスの経験値に書き込み
+                        status.experience = experience
+                        # ステータスをセーブする
+                        status.save
+                     end
+                    # 総経験値を計算するためにステータスのループをまわす
+                    total_experience = 0
+                    statuses.each do |status|
+                        total_experience += status.experience
+                    end
+                    # historyを作る ← mission_id, user_id, 総経験値
+                    history = History.new(mission_id: mission_id, user_id: @user_id, experience: total_experience)
+                    history.save
+                end
+            else
                 error = { error:"404 Not Found",detail:"invalid password" }
                 render json: error
             end
