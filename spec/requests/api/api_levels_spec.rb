a01b8381c09fad33474d28a621ca01f23877b589
tontoro <siotontoro@yahoo.co.jp>
2015-10-29 10:16:46 +0000

    csvファイル修正

--- spec/requests/api/api_levels_spec.rb
+++ spec/requests/api/api_levels_spec.rb
@@ -1,28 +1,37 @@
 require 'spec_helper'
 
-describe "Api::Levels" do
-  describe "POST /api_levels" do
-    before do
-      # userを作る
-      @user = @role.users.build(name: "Example User", email: "user@example.com",
-                                password: "foobar", password_confirmation: "foobar",
-                                card_number:"example")
-      # levelを作る
-      @level = Level.new(value:1, sufficiency:1)
-
-      # POSTの内容を作る
-      describe 'POST /api/levels.json' do
-        # headerとbodyをつくってpathにPOST
-        let(:request_header) do
-          { 'CONTENT_TYPE' => 'application/json' }
-        end
-        
-        let(:json_body) do
-          {} 
-        end
-      # 200が返ってくることをチェック
-      # jsonの中身を調べる
-      end
+describe "POST /api_levels" do
+  subject { page }
+  before do
+    @user = User.create(name: "Example User", email: "user@example.com",
+                        password: "foobar", password_confirmation: "foobar",
+                        card_number:"example")
+    # levelを作る
+    @level = Level.create(value:1, sufficiency:1)
+  end
+  
+    # POSTの内容を作る
+  describe "POST /api/levels.json" do
+    # headerとbodyをつくってpathにPOST
+    let(:request_header) do
+      { 'CONTENT_TYPE' => 'application/json' }
+    end
+      
+    let(:json_body) do
+      '{"card_number":"example","password":"foobar"}' 
+    end
+      
+    it "should return ok200" do
+      post "/api/levels.json", json_body, request_header
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+    
+    # jsonの中身を調べる
+    it "should " do
+      json = JSON.parse(response.body)
+    
+    expect(json[])
     end
   end
 end
